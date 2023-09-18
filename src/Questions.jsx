5112699c38a5982bcb4c438ea658231883bcc68b
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-09-18 11:11:01 -0400

    updated

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,13 +1,20 @@
-import SingleQuestion from "./SingleQuestion"
+import SingleQuestion from "./SingleQuestion";
 
-const Questions = ({questions}) => {
+const Questions = ({ questions, activeId, toggleQuestion }) => {
   return (
     <section className="container">
-        <h1>Questions</h1>
-        {questions.map((question) => {
-            return <SingleQuestion key={question.id} {...question}/>
-        })}
+      <h1>Questions</h1>
+      {questions.map((question) => {
+        return (
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
+      })}
     </section>
-  )
-}
-export default Questions
+  );
+};
+export default Questions;
--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const SingleQuestion = ({title, info}) => {
-
-    const [showInfo, setShowInfo] = useState(false)
-
+const SingleQuestion = ({id, title, info, activeId, toggleQuestion }) => {
+  
+const isActive = id === activeId
   return (
     <article className="question">
-        <header>
-            <h5>{title}</h5>
-        </header>
-        <p>{info}</p>
+      <header>
+        <h5>{title}</h5>
+        <button className="question-btn" onClick={() => toggleQuestion(id)}>
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isActive && <p>{info}</p>}
     </article>
-  )
-}
-export default SingleQuestion
+  );
+};
+export default SingleQuestion;
